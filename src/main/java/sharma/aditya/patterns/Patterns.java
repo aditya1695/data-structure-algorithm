e1a2266a0da670f7ebd64f6d28a6b27fc0159a52
Aditya Sharma <adity.iitkgp23@gmail.com>
2026-06-09 14:24:38 +0530

    Patterns: pushed few pattern problems

---
+++ src/main/java/sharma/aditya/patterns/Patterns.java
@@ -0,0 +1,98 @@
+package sharma.aditya.patterns;
+
+class Patterns {
+    protected void pattern1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    protected void pattern2(int n) {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    protected void pattern3(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j<=i; j++) {
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
+    protected void pattern4(int n) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j<i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    protected void pattern5(int n) {
+        for(int i = n; i > 0; i--) {
+            for(int j = 0; j<i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    protected void pattern6(int n) {
+        for(int i = 0; i<n; i++){
+            for(int j = 1; j<=n-i ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    protected void pattern7(int n) {
+        for(int i = 0; i<n; i++){
+            for(int j = 1; j<n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < 2*i+1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    protected void pattern8(int n) {
+        for(int i = n; i>0; i--){
+            for(int j = 0; j < n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < 2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    protected void pattern9(int n){
+        //first half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //second half
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/sharma/aditya/patterns/RunPattern.java
@@ -0,0 +1,12 @@
+package sharma.aditya.patterns;
+
+public class RunPattern {
+
+    public static void main(String[] args) {
+        Patterns patterns = new Patterns();
+        patterns.pattern6(4);
+        patterns.pattern7(5);
+        patterns.pattern8(5);
+        patterns.pattern9(4);
+    }
+}
